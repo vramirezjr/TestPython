4a38c996510e5c0bbb3b7d92f8117761a5bbdd0e
Victor <info@victorramirez.mx>
2022-12-01 21:24:27 +0000

    Primer archivo de Base

---
+++ testFile.py
@@ -0,0 +1,7 @@
+print('Test File')
+numeroUno = int(input('Dame un numero: '))
+numeroDos = int(input('Dame otr numero: '))
+
+Calculo = numeroUno + numeroDos
+
+print(f'El resultado de la suma es igual a: {Calculo}')
